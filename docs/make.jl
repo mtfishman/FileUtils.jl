d364d9a4f6ea406ed43ebc208c75508046cc33e1
Matt Fishman <mtfishman@users.noreply.github.com>
2026-01-08 15:58:48 -0500

    Format (#3)

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,19 @@
 using FileUtils
 using Documenter
 
-DocMeta.setdocmeta!(FileUtils, :DocTestSetup, :(using FileUtils); recursive=true)
+DocMeta.setdocmeta!(FileUtils, :DocTestSetup, :(using FileUtils); recursive = true)
 
 makedocs(;
-    modules=[FileUtils],
-    authors="Matthew Fishman <mfishman@flatironinstitute.org> and contributors",
-    repo="https://github.com/mtfishman/FileUtils.jl/blob/{commit}{path}#{line}",
-    sitename="FileUtils.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://mtfishman.github.io/FileUtils.jl",
-        assets=String[],
+    modules = [FileUtils],
+    authors = "Matthew Fishman <mfishman@flatironinstitute.org> and contributors",
+    repo = "https://github.com/mtfishman/FileUtils.jl/blob/{commit}{path}#{line}",
+    sitename = "FileUtils.jl",
+    format = Documenter.HTML(;
+        prettyurls = get(ENV, "CI", "false") == "true",
+        canonical = "https://mtfishman.github.io/FileUtils.jl",
+        assets = String[],
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/mtfishman/FileUtils.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/mtfishman/FileUtils.jl", devbranch = "main")
